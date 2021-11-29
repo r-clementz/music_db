0f1892b27f6c6baa6da9992b8b94c34f0f85ed32
r_clementz <rie.h620@gmail.com>
2021-11-29 19:35:03 +0100

    server.py added

---
+++ server.py
@@ -0,0 +1,33 @@
+from flask import Flask, jsonify
+from database import run, get
+
+app = Flask(__name__)
+
+@app.get('/users')
+def get_users():
+ users = get('SELECT * FROM users')
+ users = [dict(user) for user in users]
+ return jsonify(users)
+
+# example: localhost:5000/user/3
+@app.get('/user/<int:id>')
+def get_user_by_id(id):
+  users = get('SELECT * FROM users WHERE id = :id', { 'id': id })
+  return jsonify(dict(users[0]))
+
+#Print our all artist name
+all_artist = get ('SELECT name FROM artists')
+#Print out oldest album
+oldest_albums = get ('SELECT al_name, MIN(year_released) FROM albums JOIN name ')
+#Uppdate albums without year_released with some year
+#Adding data: add an artist 
+#Adding data: add an album to artist
+#Adding data : add a song to album
+#Deliting data: delete an artist
+#Deliting data: delete an album
+#Deliting data: delete an album
+#Average duration of songs
+#Show the longest song from each album
+#Number of songs each artist has 
+#Search artist 
+#Search songs 
